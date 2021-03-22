1406dc3c63eba85185548156e62eb11ca32a7529
Ntomboxolo1068 <ntomboxolojonas@gmail.com>
2021-03-22 20:24:30 +0200

    added task0.1.js

---
+++ task0.1.js
@@ -0,0 +1,10 @@
+var x = 0;
+var y = 1;
+console.log("the value of x:" + x);
+console.log("the value of y:" + y);
+
+ x = x + 3;
+ y = y + x;
+
+console.log("the value of x:" + x);
+console.log("the value of y:" + y);
