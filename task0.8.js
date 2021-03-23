674ab5309fae7466199d0875920c7ac1fca77fa3
Ntomboxolo1068 <ntomboxolojonas@gmail.com>
2021-03-23 17:44:46 +0200

    changes done task0.8.js

--- task0.8.js
+++ task0.8.js
@@ -3,6 +3,6 @@ function convertNumber(num){
     var min = num % 60;
     var hours = (num -min) / 60;
     
-    console.log(hours  + " " + "hour(s)" + " " + ", " + min  + " " + "minute(s)");
+    console.log(hours  + " " + "hour" + ((hours>1)?'s':'') +  ", " + min  + " " + "minute" + ((min>1)?'s':''));
 }
-convertNumber(77);
+convertNumber(133);
