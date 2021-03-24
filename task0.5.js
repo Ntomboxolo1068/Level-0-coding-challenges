76365207bc79857d0d5889417ce76259265443e1
Ntomboxolo1068 <ntomboxolojonas@gmail.com>
2021-03-24 12:20:32 +0200

    changes done task0.5.js

--- task0.5.js
+++ task0.5.js
@@ -1,8 +1,10 @@
-function areaOfTriangle(sideA,sideB,sideC)
-{
-var perimeter = (sideA + sideB + sideC) / 2;
-var area =  Math.sqrt(perimeter * ((perimeter - sideA) * (perimeter - sideB) * (perimeter - sideC)));
-console.log(area);
- return area;
+function areaOfTriangle(sideA,sideB,sideC){
+
+    var perimeter = (sideA + sideB + sideC) / 2;
+    var area =  Math.sqrt(perimeter * ((perimeter - sideA) * (perimeter - sideB) * (perimeter - sideC))
+    );
+    console.log(area);
+     return area;
 }
 areaOfTriangle(2,4,3);
+
